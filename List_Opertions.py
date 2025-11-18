52c654f07aaa7b580d099077069f05e450eb2bb9
ayushshedame-ai <ayushshedame@gmail.com>
2025-11-18 19:22:56 +0530

    Add files via upload

---
+++ List_Opertions.py
@@ -0,0 +1,45 @@
+#List Creation
+
+
+
+
+#List Element Access
+
+
+
+#Delete a list
+
+
+
+#Slicing
+
+
+
+
+#Element Change
+
+
+
+#Modifying List
+y = [1,2,4,4,3,3,3,6,5]
+y[3] = 76
+print(y)
+
+#Length of a List
+fruits = ["apple","banana","orange","strawberry","peach","kiwi","pineapple","mango","grapefruit"]
+print(len(fruits))
+
+#Append a List
+Cars = ["BMW","Audi","Mercedes","Lexus","Toyota","Ford","Chevrolet","Honda",]
+Cars.append("Maruti")
+print(Cars)
+Cars.insert(5,"Ferrari")
+print(Cars)
+Cars.remove("Ford")
+print(Cars) 
+Cars.pop()
+print(Cars)
+Cars.sort()
+print(Cars)
+Cars.reverse()
+print(Cars)
---
+++ String_Methods.py
@@ -0,0 +1,20 @@
+# Q)1 lenth of string
+A = input("Enter any String of code\n")
+print(len(A))
+
+# Q)2 
+B = input("Enter any String of code\n")
+for i in range(0,len(B)) :
+    print(B[i])
+print()
+
+# Q)4 replace space with hyphen
+C = input("Enter any String of code\n")
+c = C.replace("","-")
+print(c)
+
+# Q)6 
+B = input("Enter any String of code\n")
+for i in range(0,len(B)) :
+    print(B[i])
+print()
