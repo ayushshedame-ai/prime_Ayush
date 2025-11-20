cbfb5cd1eaeee429449d9bc7dab80a8a4a7ea4ca
ayushshedame-ai <ayushshedame@gmail.com>
2025-11-20 14:54:36 +0530

    Add files via upload

---
+++ defining a function.py
@@ -0,0 +1,21 @@
+def greet():
+    print("hello everyone")
+
+
+greet()
+def square():
+    n=(int(input("enter a number:")))
+    return n*n
+result=square()
+print(result)
+def cube():
+    n=(int(input("enter a number:")))
+    return n*n*n
+result=cube()
+print(result)
+def sum():
+    n=(int(input("enter a number:")))
+    a=(int(input("enter another number:")))
+    return n+a
+result=sum()
+print("sum=",result)
